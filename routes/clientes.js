d263501ec65520200faca839e3df6d8ed7b8501c
Vladimir Salguero <vladimirsalguero891@gmail.com>
2021-04-12 22:10:41 -0600

    Insert Clientes mysql

--- routes/clientes.js
+++ routes/clientes.js
@@ -7,4 +7,32 @@ router.get('/add', (req, res) =>{
     res.render('clientes/add',  { title: "Agregar Cliente - Bruji Rifas" });
 })
 
+router.post('/add', async(req, res) =>{
+    const { nombres, 
+        apellidos, 
+        fecha_nac, 
+        DUI, 
+        telefono, 
+        celular, 
+         
+        direccion } = req.body;
+        var datetime = new Date();
+        var fecha_registro = datetime.toISOString().slice(0,10);
+        var id_municipio = 1;
+    const newCliente = {
+        nombres, 
+        apellidos, 
+        DUI, 
+        telefono, 
+        celular, 
+        direccion,
+        id_municipio,
+        fecha_nac,          
+        fecha_registro,
+        fecha_registro
+    }
+    await pool.query('insert into clientes set ?', [newCliente]);
+    res.send('Received');
+})
+
 module.exports = router;
