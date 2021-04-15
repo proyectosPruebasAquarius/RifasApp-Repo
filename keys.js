371a24ab99071f117f50cf5d02ab21fb197706aa
Vladimir Salguero <vladimirsalguero891@gmail.com>
2021-04-14 22:09:29 -0600

    Guardar clientes, mostrar lista de clientes

--- keys.js
+++ keys.js
@@ -4,5 +4,6 @@ module.exports = {
     user: "ug4azy6jeyi3sh4j",
     password: "XgdYVm6dY3X8DI5GZ9lA",
     database: "b9kexcbzj9b0b4imcgzh",
+    dateStrings: true
   },
 };
--- routes/clientes.js
+++ routes/clientes.js
@@ -1,38 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const dateFormat = require("dateformat");
+const pool = require("../config/db.config");
 
-const pool = require('../config/db.config');
 
-router.get('/add', (req, res) =>{
-    res.render('clientes/add',  { title: "Agregar Cliente - Bruji Rifas" });
-})
+router.get("/add", (req, res) => {
+  res.render("clientes/add", { title: "Agregar Cliente - Bruji Rifas" });
+});
 
-router.post('/add', async(req, res) =>{
-    const { nombres, 
-        apellidos, 
-        fecha_nac, 
-        DUI, 
-        telefono, 
-        celular, 
-         
-        direccion } = req.body;
-        var datetime = new Date();
-        var fecha_registro = datetime.toISOString().slice(0,10);
-        var id_municipio = 1;
-    const newCliente = {
-        nombres, 
-        apellidos, 
-        DUI, 
-        telefono, 
-        celular, 
-        direccion,
-        id_municipio,
-        fecha_nac,          
-        fecha_registro,
-        fecha_registro
-    }
-    await pool.query('insert into clientes set ?', [newCliente]);
-    res.send('Received');
-})
+//ruta para guardar los clientes
+router.post("/add", async (req, res) => {
+  const {
+    nombres,
+    apellidos,
+    fecha_nac,
+    DUI,
+    telefono,
+    celular,
+    direccion,
+  } = req.body;
+  var fecha_registro = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
+  var id_municipio = 1;
+  var fecha_modificacion = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
+  const newCliente = {
+    nombres,
+    apellidos,
+    DUI,
+    telefono,
+    celular,
+    direccion,
+    id_municipio,
+    fecha_nac,
+    fecha_registro,
+    fecha_modificacion,
+  };
+  await pool.query("insert into clientes set ?", [newCliente]);
+  res.send("Received");
+});
+
+//ruta para listar los clientes
+router.get("/", async (req, res) => {
+  const clientes = await pool.query("select * from clientes");
+  res.render("clientes/list", { clientes: clientes, title: "Clientes - Bruji Rifas" });
+});
 
 module.exports = router;
