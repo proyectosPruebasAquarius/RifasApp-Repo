09935a8ba5c49ff4ffdb1543ed4e11bacffab84b
Vladimir Salguero <vladimirsalguero891@gmail.com>
2021-04-29 00:38:30 -0600

    Creando layout_auth y cambio de color en style css

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const dateFormat = require("dateformat");
+const pool = require("../config/db.config");
 
-router.get('signup/', (req, res) =>{
-    res.render('auth/signup', { title: "Agregar Usuarios - Bruji Rifas" });
+router.get("/signup", (req, res) => {
+    res.render("auth/signup", { title: "Registro de Usuarios - Bruji Rifas", layout: 'layout_auth' });
+  });
+
+router.post('signup', (req, res) =>{
+    //res.render('auth/signup', { title: "Agregar Usuarios - Bruji Rifas" });
 });
 
 
