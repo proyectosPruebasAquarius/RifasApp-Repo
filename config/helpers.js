1ba823da3e5b2abab3b36f0181a6969190ee1a69
Vladimir Salguero <vladimirsalguero891@gmail.com>
2021-05-04 01:14:27 -0600

    signup

--- config/helpers.js
+++ config/helpers.js
@@ -5,7 +5,6 @@ helpers.encryptPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     return hash;
-    
 };
 
 helpers.matchPassword = async (password, savePassword) => {
@@ -14,7 +13,6 @@ helpers.matchPassword = async (password, savePassword) => {
     } catch (error) {
         console.log(e);
     }
-
 };
 
 module.exports = helpers;
