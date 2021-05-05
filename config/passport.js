43904ea3d25e0b135b41eedc53c886a304509cea
Vladimir Salguero <vladimirsalguero891@gmail.com>
2021-05-04 23:00:49 -0600

    Signup finalizado

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,54 @@
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const pool = require("./db.config");
 const helpers = require("./helpers");
 
-passport.use('local.signup', new localStrategy({
+passport.use('local.login', new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
     passReqToCallback: true
-}, async (req, username, password, done) => {
-    //await
-    const  id_empleado  = 1;
+  }, async (req, username, password, done) => {
+    const rows = await pool.query('SELECT * FROM usuarios_admin WHERE username = ?', [username]);
+    if (rows.length > 0) {
+      const user = rows[0];
+      const validPassword = await helpers.matchPassword(password, user.password)
+      if (validPassword) {
+        done(null, user, req.flash('success', 'Welcome ' + user.username));
+      } else {
+        done(null, false, req.flash('message', 'Incorrect Password'));
+      }
+    } else {
+      return done(null, false, req.flash('message', 'The Username does not exists.'));
+    }
+  }));
+  
+  passport.use('local.signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, async (req, username, password, done) => {
+  
+    const id_empleado = 1;
     const estado = 1;
-    const newUser = {
-        username,
-        password,
-        id_empleado,
-        estado
+    let newUser = {
+      
+      username,
+      password,
+      id_empleado,
+      estado
     };
     newUser.password = await helpers.encryptPassword(password);
-    const result = await pool.query('insert into usuarios_admin set ?', [newUser]);
+    // Saving in the Database
+    const result = await pool.query('INSERT INTO usuarios_admin SET ? ', newUser);
     newUser.id = result.insertId;
     return done(null, newUser);
-}));
-
-passport.serializeUser((user, done) => {
-   done(null, user.id);
-});
-
-passport.deserializeUser(async(id, done) => {
-    const rows = await pool.query('select * from usuarios_admin where id = ?', id);
+  }));
+  
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+    const rows = await pool.query('SELECT * FROM usuarios_admin WHERE id = ?', [id]);
     done(null, rows[0]);
-})
+  });
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const dateFormat = require("dateformat");
 const pool = require("../config/db.config");
 
 const passport = require("passport");
@@ -10,16 +9,36 @@ router.get("/signup", (req, res) => {
     res.render("auth/signup", { title: "Registro de Usuarios - Bruji Rifas" });
   });
 
-router.post('/signup', (req, res) =>{
-    passport.authenticate('local.signup', {
-      successRedirect : '/',
-      failureRedirect : '/login',
-      failureFlash : true
-    });
-});
+  router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true
+  }));
+  
+  // SINGIN
+  router.get('/login', (req, res) => {
+    res.render("auth/login", { title: "Login - Bruji Rifas", layout: 'layout_auth' });
+  });
 
-router.get("/login", (req, res) => {
-  res.render("auth/login", { title: "Login - Bruji Rifas", layout: 'layout_auth' });
-});
+  router.post('/login', (req, res, next) => {
+    req.check('username', 'Username is Required').notEmpty();
+    req.check('password', 'Password is Required').notEmpty();
+    const errors = req.validationErrors();
+    if (errors.length > 0) {
+      req.flash('message', errors[0].msg);
+      res.redirect('/login');
+    }
+    passport.authenticate('local.login', {
+      successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: true
+    })(req, res, next);
+  });
+  
+  router.get('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/');
+  });
+
 
 module.exports = router;
