02eb3b5170654ce3bfa85af56e356ad71dca4b8b
Vladimir Salguero <vladimirsalguero891@gmail.com>
2021-05-24 22:21:45 -0600

    Adding moment for date format

--- routes/municipios.js
+++ routes/municipios.js
@@ -4,11 +4,11 @@ const dateFormat = require("dateformat");
 const pool = require("../config/db.config");
 const { isLoggedIn } = require('../config/auth');
 
-//ruta para listar los municipios
+//ruta para listar los municipios en select2
 router.get("/list", async (req, res) => {
-    const nombre = req.query.q;
-    const municipios = await pool.query("select id, nombre as text from municipios where nombre like '%" + nombre + "%'");
-    res.send(municipios);
-  });
-
-  module.exports = router;
+   const nombre = req.query.q;
+   const municipios = await 
+   pool.query("select a.id, concat(a.nombre,' - ', b.nombre) as text from municipios as a inner join departamentos as b on a.id_departamento = b.id where a.nombre like '%" + nombre + "%'");
+   res.send(municipios);
+});
+module.exports = router;
